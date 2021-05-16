046d7270d52ccc277d410167612169b78428f92b
HasinduLanka <hlrathnayaka@gmail.com>
2021-05-16 17:28:56 +0530

    generic sw

--- service-worker.js
+++ service-worker.js
@@ -15,54 +15,51 @@ const PRECACHE_URLS = [
     './'
 ];
 
+
 async function onInstall(event) {
     console.info('Service worker: Install');
 
-    // Fetch and cache all matching items from the assets manifest
-    const assetsRequests = self.assetsManifest.assets
-        .filter(asset => offlineAssetsInclude.some(pattern => pattern.test(asset.url)))
-        .filter(asset => !offlineAssetsExclude.some(pattern => pattern.test(asset.url)))
-        .map(asset => new Request(asset.url, { integrity: asset.hash, redirect: 'follow' }));
-
-    che = await caches.open(cacheName);
-    // che.addAll(PRECACHE_URLS);
-    che.addAll(assetsRequests);
-
-    var indexrequest = new Request('/index.html', { redirect: 'follow', cache: 'no-cache' });
-    fetch(indexrequest).then(response => {
-        // Put a copy of the response in the runtime cache.
-        che.put(new Request('/'), response.clone());
-        che.put(new Request('index.html'), response.clone());
-    });
-    // await caches.open(cacheName).then(cache => cache.addAll(assetsRequests));
+    event.waitUntil(
+        caches.open(PRECACHE)
+            .then(cache => cache.addAll(PRECACHE_URLS))
+            .then(self.skipWaiting())
+    );
 }
 
 async function onActivate(event) {
     console.info('Service worker: Activate');
 
-    // Delete unused caches
-    const cacheKeys = await caches.keys();
-    await Promise.all(cacheKeys
-        .filter(key => key.startsWith(cacheNamePrefix) && key !== cacheName)
-        .map(key => caches.delete(key)));
+    const currentCaches = [PRECACHE, RUNTIME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
+        }).then(cachesToDelete => {
+            return Promise.all(cachesToDelete.map(cacheToDelete => {
+                return caches.delete(cacheToDelete);
+            }));
+        }).then(() => self.clients.claim())
+    );
 }
 
 async function onFetch(event) {
-    let cachedResponse = null;
-    if (event.request.method === 'GET') {
-        // For all navigation requests, try to serve index.html from cache
-        // If you need some URLs to be server-rendered, edit the following check to exclude those URLs
-        const shouldServeIndexHtml = event.request.mode === 'navigate';
+    // Skip cross-origin requests, like those for Google Analytics.
+    if (event.request.url.startsWith(self.location.origin)) {
+        event.respondWith(
+            caches.match(event.request).then(cachedResponse => {
+                if (cachedResponse) {
+                    return cachedResponse;
+                }
 
-        const request = shouldServeIndexHtml ? 'index.html' : event.request;
-        const cache = await caches.open(cacheName);
-        cachedResponse = await cache.match(request);
-        if (cachedResponse && cachedResponse.redirected) {
-            cachedResponse = false;
-            console.log("Denied req : " + request);
-        }
+                return caches.open(RUNTIME).then(cache => {
+                    return fetch(event.request).then(response => {
+                        // Put a copy of the response in the runtime cache.
+                        return cache.put(event.request, response.clone()).then(() => {
+                            return response;
+                        });
+                    });
+                });
+            })
+        );
     }
-
-    return cachedResponse || fetch(event.request);
 }
 /* Manifest version: UtOy0rqP */
